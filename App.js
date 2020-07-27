daf28298d0ea0a99a44330324f537d6e36e1296a
Brian Artea <brian@expertdesign.cc>
2020-07-27 17:33:54 -0400

    Inline Styles & StyleSheet Objects

--- App.js
+++ App.js
@@ -1,70 +1,33 @@
-// import React from 'react';
-// import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={{ padding: 70 }}>
-//       <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-//         <TextInput placeholder="Course Goals" style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10 }} />
-//         <Button title="ADD" />
-//       </View>
-//       <View>
-
-//       </View>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-
-// });
-
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View
-      style={{
-        padding: 50,
-        flexDirection: 'row',
-        width: '80%',
-        height: 300,
-        justifyContent: 'space-evenly',
-        alignItems: 'stretch',
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: 'red',
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>1</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goals" style={styles.input} />
+        <Button title="ADD" />
       </View>
-      <View
-        style={{
-          backgroundColor: 'blue',
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>2</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: 'green',
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>3</Text>
+      <View>
+
       </View>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  screen: {
+    padding: 70
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  }
+});
